ad34091c9655abfa0c7102b73d1178107ebb1a1c
wabwi <wabwiibrahim@gmail.com>
2022-11-29 21:20:38 -0800

    The output of the program should be:
    The string Last digit of, followed by
    
    the number, followed by
    
    the string is, followed by the last digit of number, followed by
    
    if the last digit is greater than 5: the string and is greater than 5
    
    if the last digit is 0: the string and is 0
    
    if the last digit is less than 6 and not 0: the string and is less than 6 and not 0
    
    followed by a new line

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+
+print("Last digit of" + str(number) + "is" + str(number % 10), end='' )
+
+if number > 5:
+    print("and is greater than 5")
+
+if number == 0:
+    print("and is 0")
+
+if number < 6 and number != 0:
+    print("and is less than 6 and not 0")
