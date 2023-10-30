e906d285bfb103a78398991be44550e706c4177d
Wabwi <wabwiibrahim@gmail.com>
2023-10-30 22:32:09 +0300

    updated docstring

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""
+Created on Oct 2023
+
+@author: Wabwi Ibrahim
+"""
--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Oct 2023
 
+@author: Wabwi Ibrahim
+"""
 
 
 class Rectangle:
