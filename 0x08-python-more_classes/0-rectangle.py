6b44205a77f344eee16f64d02133d601e0cef4e4
Wabwi <wabwiibrahim@gmail.com>
2023-10-30 22:45:56 +0300

    added new line

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -6,12 +6,12 @@ Created on Oct 2023
 @author: Wabwi Ibrahim
 """
 
+
 class Rectangle:
     """Empty class Rectangle that defines a rectangle figure
 
     Attributes:
         empty
     """
-
 
     pass
