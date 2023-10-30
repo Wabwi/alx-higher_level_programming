49e090b7693d798eaa8a01b4c1d8eafbdca0a5b0
Wabwi <wabwiibrahim@gmail.com>
2023-10-30 17:29:34 +0300

    Write an empty class Rectangle that defines a rectangle:
    You are not allowed to import any module

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
